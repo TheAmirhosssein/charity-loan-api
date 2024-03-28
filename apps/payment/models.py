9d73936154f4f9abf746da391ceebf5d037c7769
amirhossein <amirhosseinmobini9@gmail.com>
2024-03-28 16:43:41 +0000

    ✨feature: payment request added

--- apps/payment/models.py
+++ apps/payment/models.py
@@ -21,7 +21,7 @@ class Payment(BaseModel):
     payment_price = models.PositiveBigIntegerField(_("payment price"))
     payment_date = models.DateField(_("payment date"), default=timezone.now)
     payment_type = models.CharField(_("payment type"), choices=TYPES, max_length=5)
-    is_valid = models.BooleanField(_("is valid"), default=True)
+    other_info = models.JSONField(default=dict)
 
     @property
     def payment_date_jalali(self):
@@ -29,3 +29,36 @@ class Payment(BaseModel):
 
     def __str__(self) -> str:
         return f"{self.user.get_full_name()} | {self.payment_date_jalali}"
+
+
+class PaymentRequest(BaseModel):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="user_payment_request",
+        verbose_name=_("user"),
+    )
+    paid_price = models.PositiveBigIntegerField(_("payment price"))
+    paid_date = models.DateField(_("payment date"))
+    is_confirmed = models.BooleanField(_("is confirmed"), default=False)
+
+    @property
+    def paid_date_jalali(self):
+        return date2jalali(self.paid_date).strftime("%Y/%m/%d")
+
+    def confirm(self):
+        if not self.confirm:
+            Payment.objects.create(
+                user=self.user,
+                payment_date=self.paid_date,
+                payment_price=self.paid_price,
+                payment_type="MA",
+                other_info={"request_id": self.pk},
+            )
+
+    def undo_confirm(self):
+        if self.confirm:
+            Payment.objects.filter(other_info__request_id=self.pk).delete()
+
+    def __str__(self) -> str:
+        return f"{self.user.get_full_name()} | {self.paid_date}"
