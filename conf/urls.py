8631444800c4fa89a81b8e216b7826fdd7ac116f
amirhossein <amirhosseinmobini9@gmail.com>
2024-03-29 18:13:36 +0000

    ✨feature: media added to urls

--- conf/urls.py
+++ conf/urls.py
@@ -1,7 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("apps.api.urls")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
