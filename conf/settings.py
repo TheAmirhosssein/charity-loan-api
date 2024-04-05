14956625c9eda18d916929dd1bcc876e98505601
amirhossein <amirhosseinmobini9@gmail.com>
2024-04-05 16:17:51 +0000

    ✨feature: language edited

--- conf/settings.py
+++ conf/settings.py
@@ -115,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "fa-ir"
 
 TIME_ZONE = "Asia/Tehran"
 
